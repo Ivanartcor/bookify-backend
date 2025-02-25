8d9fb1850c9afe505bcc7b8c1148a77972058822
Iván <ivanartcor@gmail.com>
2025-02-25 14:45:01 +0100

    venga

--- src/companies/companies.controller.ts
+++ src/companies/companies.controller.ts
@@ -1,8 +1,13 @@
 import {
     Controller, Get, Post, Put, Delete, Param, Body,
+    UseInterceptors,
+    UploadedFile,
 } from '@nestjs/common';
 import { CompaniesService } from './companies.service';
 import { Company } from './company.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { extname } from 'path';
+import { diskStorage } from 'multer';
 
 @Controller('companies')
 export class CompaniesController {
@@ -47,4 +52,40 @@ export class CompaniesController {
   async getCompaniesByCity(@Param('cityId') cityId: number): Promise<Company[]> {
     return await this.companiesService.findByCity(cityId);
   }
+
+
+
+   /** ✅ Subir o actualizar el logo de la empresa */
+   @Put(':id/logo')
+   @UseInterceptors(FileInterceptor('file', {
+       storage: diskStorage({
+           destination: './uploads',
+           filename: (req, file, callback) => {
+               const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+               const fileExtName = extname(file.originalname);
+               callback(null, `logo-${uniqueSuffix}${fileExtName}`);
+           },
+       }),
+   }))
+   async uploadLogo(@Param('id') id: number, @UploadedFile() file: Express.Multer.File) {
+       const filePath = `http://localhost:3000/uploads/${file.filename}`;
+       return await this.companiesService.update(id, { logo: filePath });
+   }
+
+   /** ✅ Subir o actualizar el banner de la empresa */
+   @Put(':id/banner')
+   @UseInterceptors(FileInterceptor('file', {
+       storage: diskStorage({
+           destination: './uploads',
+           filename: (req, file, callback) => {
+               const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
+               const fileExtName = extname(file.originalname);
+               callback(null, `banner-${uniqueSuffix}${fileExtName}`);
+           },
+       }),
+   }))
+   async uploadBanner(@Param('id') id: number, @UploadedFile() file: Express.Multer.File) {
+       const filePath = `http://localhost:3000/uploads/${file.filename}`;
+       return await this.companiesService.update(id, { banner_image: filePath });
+   }
 }
